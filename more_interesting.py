f29f85cdc0cf39caeeaeb17b9e45009b2b41bf49
Jerónimo Fotinós <jerofoti@gmail.com>
2023-03-26 14:01:44 -0300

    Fixed error: Csc reproduction was using prob_stem instead of self.prob_stem

--- more_interesting.py
+++ more_interesting.py
@@ -1,7 +1,7 @@
 from cells import *
 from culture import *
 
-csc_culture = Culture(first_cell_type='csc')
-csc_culture.simulate(10)
+csc_culture = Culture(first_cell_type='csc', prob_stem=0.5, cell_max_repro_attempts=1000)
+csc_culture.simulate(7)
 print(len(csc_culture.cells))
 csc_culture.plot_culture_spheres()
