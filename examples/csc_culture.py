c999c917bb601c6162ad9682b650f0a9265c57ba
Jerónimo Fotinós <jerofoti@gmail.com>
2023-04-02 19:43:08 -0300

    All tests pass, adapted examples and animation method to the changes

--- examples/csc_culture.py
+++ examples/csc_culture.py
@@ -2,7 +2,7 @@ from tumorsphere.cells import *
 from tumorsphere.culture import *
 
 csc_culture = Culture(
-    first_cell_type="csc", prob_stem=0.5, cell_max_repro_attempts=1000
+    first_cell_is_stem=True, prob_stem=0.5, cell_max_repro_attempts=1000
 )
 csc_culture.simulate(7)
 print(len(csc_culture.cells))
