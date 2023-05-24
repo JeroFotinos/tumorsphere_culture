161bf7a833ec0cf8d3e7fe0263932345731261d6
Jerónimo Fotinós <jerofoti@gmail.com>
2023-05-24 10:56:29 -0300

    Processed sim 3, made first version of script for Osaka and CCAD.

---
+++ data/sim_6_long_time_ps_next_to_0.7/submit_simulation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -q seq_long
+#$ -N tumorsph_sim_6
+#$ -o log/tumorsph_sim_6
+#$ -e log/tumorsph_sim_6
+#$ -l m_mem_free=2G
+#$ -cwd
+#$ -pe smp 50 ### we request the number of cores
+
+
+### Script to submit the simulation.
+### You can execute it by doing:
+### qsub ./submit_simulation.sh
+
+source ~/miniconda3/bin/activate
+conda activate
+python -m tumorsphere.cli --prob-stem "0.68,0.70,0.72,0.74,0.76" --prob-diff "0" --realizations 10 --steps-per-realization 100 --rng-seed 1292317634567
