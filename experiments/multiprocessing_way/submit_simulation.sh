21d2f87073ffb1853ef703bcd601d79154ebfef2
Jerónimo Fotinós <jerofoti@gmail.com>
2023-05-24 10:56:29 -0300

    Processed sim 3, made first version of script for Osaka and CCAD.

--- experiments/multiprocessing_way/submit_simulation.sh
+++ experiments/multiprocessing_way/submit_simulation.sh
@@ -4,4 +4,4 @@
 ### You can execute it by doing:
 ### qsub ./submit_simulation.sh
 
-python3 -m tumorsphere.cli --prob-stem 0.5 0.3 0.8 --prob-diff 0.2 0.4 0.6 --realizations 10 --steps-per-realization 10 --rng-seed 1234567
+python3 -m tumorsphere.cli --prob-stem "0.5,0.3,0.8" --prob-diff "0.2,0.4,0.6" --realizations 10 --steps-per-realization 10 --rng-seed 1292317634567
