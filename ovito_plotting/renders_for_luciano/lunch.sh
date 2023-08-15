78ee3818b868120464dee837821863c6f89be38d
Nate Keel <nate@pop-os.localdomain>
2023-08-15 17:28:03 -0300

    Renders for Luciano.

---
+++ ovito_plotting/renders_for_luciano/lunch.sh
@@ -0,0 +1,2 @@
+# python -m tumorsphere.cli --prob-stem "0.20,0.70,0.90" --prob-diff "0" --realizations 1 --steps-per-realization 25 --rng-seed 1292317634567 --parallel-processes 3 --ovito True
+python -m tumorsphere.cli --prob-stem "0.4" --prob-diff "0" --realizations 1 --steps-per-realization 21 --rng-seed 1292317634567 --parallel-processes 1 --ovito True
