7a3bc78aac8b7bb060637695a2577502b935bc2a
Jerónimo Fotinós <jerofoti@gmail.com>
2024-03-11 23:21:49 -0300

    Added tests for db_file_compararer.py > compare_databases, and its CLI, are-dbs-equal.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 
 import pytest
+import sqlite3
+import tempfile
+import subprocess
 
 from tumorsphere.core.culture import Culture
 
@@ -122,3 +125,32 @@ def culture_evolved_at_time_3():
             if os.path.exists(db_file):
                 os.remove(db_file)
     return csc_culture
+
+
+# -------- For testing the `db_file_comparer.py` module --------
+
+@pytest.fixture
+def temp_db():
+    """Fixture to create a temporary database file with specific content."""
+    def _temp_db(schema, data):
+        temp_db = tempfile.NamedTemporaryFile(delete=False)
+        conn = sqlite3.connect(temp_db.name)
+        cursor = conn.cursor()
+        for table, create_statement in schema.items():
+            cursor.execute(create_statement)
+            for row in data.get(table, []):
+                cursor.execute(f"INSERT INTO {table} VALUES ({','.join('?' for _ in row)})", row)
+        conn.commit()
+        conn.close()
+        return temp_db.name
+    return _temp_db
+
+# -------- For testing the CLI of the `db_file_comparer.py` module --------
+
+@pytest.fixture(scope="session")
+def run_cli():
+    """Fixture to run a CLI command and return its output, error, and exit status."""
+    def _run_cli(command, cwd=None):
+        result = subprocess.run(command, capture_output=True, text=True, shell=True, cwd=cwd)
+        return result.stdout.strip(), result.stderr.strip(), result.returncode
+    return _run_cli
---
+++ tests/test_library/test_db_file_comparer.py
@@ -0,0 +1,25 @@
+from tumorsphere.library.db_file_comparer import compare_databases
+from unittest.mock import patch
+
+def test_compare_databases_equal(temp_db):
+    schema = {"Cells": "CREATE TABLE Cells (id INTEGER, attr1 INTEGER, attr2 TEXT)"}
+    data = {"Cells": [(1, 10, 'A'), (2, 20, 'B')]}
+
+    db_path1 = temp_db(schema, data)
+    db_path2 = temp_db(schema, data)  # Creating a second, identical temp db
+
+    with patch('builtins.print') as mock_print:
+        assert compare_databases(db_path1, db_path2) == True
+        mock_print.assert_called_with("Databases are equal.")
+
+def test_compare_databases_not_equal(temp_db):
+    schema = {"Cells": "CREATE TABLE Cells (id INTEGER, attr1 INTEGER, attr2 TEXT)"}
+    data_db1 = {"Cells": [(1, 10, 'A'), (2, 20, 'B')]}
+    data_db2 = {"Cells": [(1, 10, 'A'), (2, 20, 'C')]}
+
+    db_path1 = temp_db(schema, data_db1)
+    db_path2 = temp_db(schema, data_db2)
+
+    with patch('builtins.print') as mock_print:
+        assert compare_databases(db_path1, db_path2) == False
+        mock_print.assert_any_call("Data in table Cells is different.")
