2a0b4619c8c8689a821b0eb65370652b311ae5f2
Jerónimo Fotinós <jerofoti@gmail.com>
2024-03-13 17:51:35 -0300

    Tests for tumorsphere status.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,44 @@ from pathlib import Path
 # ---------- tumorsphere status command ----------
 
 
+def test_status_cli_with_db_files(run_cli):
+    cwd = Path(__file__).parent.resolve()
+    data_dir = "data/testing_status_cli"
+
+    command = f"tumorsphere status --data-dir {data_dir}"
+    stdout, stderr, returncode = run_cli(command, cwd=str(cwd))
+
+    assert returncode == 0, "Command failed with an error"
+    expected_output = (
+        "10 steps for culture pd=0.0, ps=0.3, seed=322927469740366"
+    )
+    assert (
+        expected_output in stdout
+    ), (
+        "Expected output indicating steps for .db files was not found. "
+        f"Found: {stdout}"
+    )
+
+
+def test_status_cli_with_dat_files(run_cli):
+    cwd = Path(__file__).parent.resolve()
+    data_dir = "data/testing_status_cli"
+
+    command = f"tumorsphere status --data-dir {data_dir} --dat-files True"
+    stdout, stderr, returncode = run_cli(command, cwd=str(cwd))
+
+    assert returncode == 0, "Command failed with an error"
+    expected_output = (
+        "culture_pd=0.0_ps=0.3_rng_seed=322927469740366.dat: 10 steps."
+    )
+    assert (
+        expected_output in stdout
+    ), (
+        "Expected output indicating steps for .dat files was not found. "
+        f"Found: {stdout}"
+    )
+
+
 # ---------- tumorsphere mergedbs command ----------
 
 
@@ -34,7 +72,7 @@ def test_cli_are_dbs_equal__case_databases_are_equal(run_cli):
     ), "Expected output indicating databases are equal was not found"
 
 
-def test_cli_databases_are_different(run_cli):
+def test_cli_are_dbs_equal__case_databases_are_different(run_cli):
     # parent gives me the directory in which the file is located, in this case
     # the directory tests
     cwd = Path(__file__).parent.resolve()
