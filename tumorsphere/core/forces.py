ca1a57de38db21e6cb68a9c73899bbfc1c2381bb
gonzaloangaut <angaut13@gmail.com>
2024-11-08 00:34:56 -0300

    working on forces

--- tumorsphere/core/forces.py
+++ tumorsphere/core/forces.py
@@ -15,7 +15,7 @@ class Force(ABC):
         cells,
         phies,
         cell_index,
-        significant_neighbors,
+        relative_positions,
         delta_t,
         area,
     ):
@@ -118,12 +118,17 @@ class Vicsek(Force):
     ):
         # In this model there is no change in the velocity but in the orientation
         cell = cells[cell_index]
-        dif_position = (cell.velocity() + np.array([0, 0, 0]))*delta_t
-        alpha = np.arctan2(
-            np.sin(phies[cell_index]) + np.sin(phies[neighbor_index]),
-            np.cos(phies[cell_index]) + np.cos(phies[neighbor_index]),
-        )
-        dif_phi = alpha - phies[cell_index]
+        # initialization of the parameters of interaction
+        dif_phi = 0
+        # Calculate interaction with filtered neighbors
+        for neighbor_index, relative_pos in significant_neighbors:
+            alpha = np.arctan2(
+                np.sin(phies[cell_index]) + np.sin(phies[neighbor_index]),
+                np.cos(phies[cell_index]) + np.cos(phies[neighbor_index]),
+            )
+            dif_phi_2 = alpha - phies[cell_index]
+            dif_phi += dif_phi_2
+        dif_position = cell.velocity()*delta_t
         return dif_position, dif_phi
 
 
@@ -148,17 +153,28 @@ class Vicsek_and_Spring_Force(Force):
         area,
     ):
         cell = cells[cell_index]
-        # We first calculate the force
-        fx = -self.k_spring_force * (-relative_pos_x)  # OJO SIGNO
-        fy = -self.k_spring_force * (-relative_pos_y)
+        # initialization of the parameters of interaction
+        dif_phi = 0
+        dif_velocity = np.zeros(3)
+        # Calculate interaction with filtered neighbors
+        for neighbor_index, relative_pos in significant_neighbors:
+            relative_pos_x, relative_pos_y = relative_pos
+            # We first calculate the force
+            fx = -self.k_spring_force * (-relative_pos_x)  # OJO SIGNO
+            fy = -self.k_spring_force * (-relative_pos_y)
+            # Calculate change in velocity given by the force model
+            dif_velocity_2 = np.array([fx, fy, 0])
+            # Accumulate changes in velocity
+            dif_velocity += dif_velocity_2
+            # and the change in the orientation is given by Vicsek
+            alpha = np.arctan2(
+                np.sin(phies[cell_index]) + np.sin(phies[neighbor_index]),
+                np.cos(phies[cell_index]) + np.cos(phies[neighbor_index]),
+            )
+            dif_phi_2 = alpha - phies[cell_index]
+            dif_phi += dif_phi_2
         # In this model the change in velocity is equal to the force
         dif_position = (cell.velocity() + np.array([fx, fy, 0]))*delta_t
-        # and the change in the orientation is given by Vicsek
-        alpha = np.arctan2(
-            np.sin(phies[cell_index]) + np.sin(phies[neighbor_index]),
-            np.cos(phies[cell_index]) + np.cos(phies[neighbor_index]),
-        )
-        dif_phi = alpha - phies[cell_index]
         return dif_position, dif_phi
 
 
@@ -342,155 +358,6 @@ class Grosmann(Force):
         dif_position = (cell.velocity()+dif_velocity)*delta_t
         # and the change in the orientation:
         dif_phi = mR * torque * delta_t
-        #dif_position = (cell.velocity()+dif_velocity)*delta_t
-        # we first calculate the kernel, using f[ξ]=ξ**gamma. ξ=xi from the paper
-        # We introduce some matrix/vectors/parameters that we need
-
-        # # nematic matrix
-        # Q_cell = np.array(
-        #     [
-        #         [
-        #             np.cos(2 * phies[cell_index]),
-        #             np.sin(2 * phies[cell_index]),
-        #             0,
-        #         ],
-        #         [
-        #             np.sin(2 * phies[cell_index]),
-        #             -np.cos(2 * phies[cell_index]),
-        #             0,
-        #         ],
-        #         [0, 0, 0],
-        #     ]
-        # )
-
-        # Q_neighbor = np.array(
-        #     [
-        #         [
-        #             np.cos(2 * phies[neighbor_index]),
-        #             np.sin(2 * phies[neighbor_index]),
-        #             0,
-        #         ],
-        #         [
-        #             np.sin(2 * phies[neighbor_index]),
-        #             -np.cos(2 * phies[neighbor_index]),
-        #             0,
-        #         ],
-        #         [0, 0, 0],
-        #     ]
-        # )
-
-        # # mean nematic matrix
-        # mean_nematic = (1 / 2) * (Q_cell + Q_neighbor)
-        # # relative position and angle
-        # relative_pos = np.array([relative_pos_x, relative_pos_y, 0])
-        # relative_angle = phies[cell_index] - phies[neighbor_index]
-
-        # # anisotropy
-        # eps = (cell.aspect_ratio**2 - 1) / (cell.aspect_ratio**2 + 1)
-        # # diagonal squared
-        # diag2 = (area / np.pi) * (cell.aspect_ratio + 1 / cell.aspect_ratio)
-
-        # # and now we can calculate xi
-        # xi = np.exp(
-        #     -1
-        #     * np.matmul(
-        #         relative_pos,
-        #         (np.matmul(np.identity(3) - eps * mean_nematic, relative_pos)),
-        #     )
-        #     / (2 * (1 - eps**2 * (np.cos(relative_angle)) ** 2) * diag2)
-        # )
-
-        # # the kernel is: (k_rep = k, b_exp=gamma (from the paper))
-        # kernel = (self.kRep * self.bExp * xi**self.bExp) / (
-        #     diag2 * (1 - eps**2 * (np.cos(relative_angle)) ** 2)
-        # )
-
-        # # finally we can calculate the force:
-        # force = kernel * np.matmul(
-        #     np.identity(3) - eps * mean_nematic, relative_pos
-        # )
-
-        # # On the other way, we calculate the torque
-        # # we introduce the theta = angle of r_kj
-        # theta = np.arctan2(relative_pos_y, relative_pos_x)
-        # torque = (kernel / 2) * (
-        #     eps
-        #     * np.linalg.norm(relative_pos)
-        #     ** 2  # (relative_pos_x**2 + relative_pos_y**2)
-        #     * np.sin(2 * (phies[cell_index] - theta))
-        #     + eps**2
-        #     * (
-        #         np.matmul(
-        #             relative_pos,
-        #             (
-        #                 np.matmul(
-        #                     np.identity(3) - eps * mean_nematic, relative_pos
-        #                 )
-        #             ),
-        #         )
-        #         * np.sin(2 * (-relative_angle))
-        #         / (1 - eps**2 * (np.cos(relative_angle)) ** 2)
-        #     )
-        # )
-
-        # Now that we have the force and torque we can calculate the change in velocity
-        # and orientation as it is done in the paper. Becuase of this, we need the Q
-        # matrix (already calculated) and the mobilities
-
-        # # longitudinal & transversal mobility
-        # if np.isclose(cell.aspect_ratio, 1):
-        #     mP = 1 / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        #     mS = 1 / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        # else:
-        #     mP = (
-        #         1
-        #         / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        #         * (3 * cell.aspect_ratio / 4.0)
-        #         * (
-        #             (cell.aspect_ratio) / (1 - cell.aspect_ratio**2)
-        #             + (2.0 * cell.aspect_ratio**2 - 1.0)
-        #             / np.power(cell.aspect_ratio**2 - 1.0, 1.5)
-        #             * np.log(
-        #                 cell.aspect_ratio + np.sqrt(cell.aspect_ratio**2 - 1.0)
-        #             )
-        #         )
-        #     )
-        #     mS = (
-        #         1
-        #         / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        #         * (3 * cell.aspect_ratio / 8.0)
-        #         * (
-        #             (cell.aspect_ratio) / (cell.aspect_ratio**2 - 1.0)
-        #             + (2.0 * cell.aspect_ratio**2 - 3.0)
-        #             / np.power(cell.aspect_ratio**2 - 1.0, 1.5)
-        #             * np.log(
-        #                 cell.aspect_ratio + np.sqrt(cell.aspect_ratio**2 - 1.0)
-        #             )
-        #         )
-        #     )
-
-        # # rotational mobility
-        # mR = (
-        #     3
-        #     / (
-        #         2
-        #         * (area / np.pi)
-        #         * (cell.aspect_ratio + 1 / cell.aspect_ratio)
-        #     )
-        #     * mP
-        # )
-
-        # then the change in the velocity is given by:
-        # dif_velocity = np.matmul(
-        #     ((mP + mS) / 2) * np.identity(3) + ((mP - mS) / 2) * Q_cell,
-        #     force,
-        # )
-        # # we calculate the change in the position of the cell, given all the neighbors.
-        # # Remember that the intrinsic velocity is already multiplied by the mobility
-        # # (Like in Grosmann paper).
-        # dif_position = (cell.velocity()+dif_velocity)*delta_t
-        # # and the change in the orientation:
-        # dif_phi = mR * torque * delta_t
         return dif_position, dif_phi
 
 
@@ -728,198 +595,4 @@ class Anisotropic_Grosmann(Force):
         nP = s_nP*np.random.normal(0, 1)
         nS = s_nS*np.random.normal(0, 1)
         noise = nP*direction_vector+nS*perpendicular_vector
-        # nematic matrix
-        # Q_cell = np.array(
-        #     [
-        #         [
-        #             np.cos(2 * phies[cell_index]),
-        #             np.sin(2 * phies[cell_index]),
-        #             0,
-        #         ],
-        #         [
-        #             np.sin(2 * phies[cell_index]),
-        #             -np.cos(2 * phies[cell_index]),
-        #             0,
-        #         ],
-        #         [0, 0, 0],
-        #     ]
-        # )
-
-        # Q_neighbor = np.array(
-        #     [
-        #         [
-        #             np.cos(2 * phies[neighbor_index]),
-        #             np.sin(2 * phies[neighbor_index]),
-        #             0,
-        #         ],
-        #         [
-        #             np.sin(2 * phies[neighbor_index]),
-        #             -np.cos(2 * phies[neighbor_index]),
-        #             0,
-        #         ],
-        #         [0, 0, 0],
-        #     ]
-        # )
-
-        # # relative position and angle
-        # relative_pos = np.array([relative_pos_x, relative_pos_y, 0])
-        # relative_angle = phies[cell_index] - phies[neighbor_index]
-
-        # # anisotropy
-        # # eps_cell = (cell.aspect_ratio**2 - 1) / (cell.aspect_ratio**2 + 1)
-        # eps_neighbor = (neighbor.aspect_ratio**2 - 1) / (
-        #     neighbor.aspect_ratio**2 + 1
-        # )
-        # # diagonal squared (what we call alpha)
-        # # alpha_cell = (area / np.pi) * (
-        # #     cell.aspect_ratio + 1 / cell.aspect_ratio
-        # # )
-        # alpha_neighbor = (area / np.pi) * (
-        #     neighbor.aspect_ratio + 1 / neighbor.aspect_ratio
-        # )
-
-        # # we now calculate the mean nematic matrix (different than before) (the matrix M)
-        # matrix_M = (
-        #     alpha_cell * eps_cell * Q_cell
-        #     + alpha_neighbor * eps_neighbor * Q_neighbor
-        # ) / (alpha_cell + alpha_neighbor)
-
-        # # now we introduce the constant beta introduced by us in the TF
-        # beta = (
-        #     (alpha_cell + alpha_neighbor) ** 2
-        #     - (alpha_cell * eps_cell - alpha_neighbor * eps_neighbor) ** 2
-        #     - 4
-        #     * alpha_cell
-        #     * eps_cell
-        #     * alpha_neighbor
-        #     * eps_neighbor
-        #     * (np.cos(relative_angle)) ** 2
-        # )
-
-        # # calculate the kernel, using f[ξ]=ξ**gamma. ξ=xi calculated
-        # # and now we can calculate xi
-        # xi = np.exp(
-        #     -1
-        #     * ((alpha_cell + alpha_neighbor) / beta)
-        #     * np.matmul(
-        #         relative_pos,
-        #         np.matmul(np.identity(3) - matrix_M, relative_pos),
-        #     )
-        # )
-
-        # # the kernel is: (k_rep = k, b_exp=gamma (from the paper))
-        # kernel = (
-        #     2
-        #     * self.kRep
-        #     * self.bExp
-        #     * xi**self.bExp
-        #     * ((alpha_cell + alpha_neighbor) / beta)
-        # )
-
-        # # finally we can calculate the force:
-        # force = kernel * np.matmul(np.identity(3) - matrix_M, relative_pos)
-
-        # # On the other way, we calculate the torque
-        # # we introduce the theta=angle of r_kj
-        # theta = np.arctan2(relative_pos_y, relative_pos_x)
-        # torque = kernel * (
-        #     (
-        #         (
-        #             2
-        #             * alpha_cell
-        #             * eps_cell
-        #             * alpha_neighbor
-        #             * eps_neighbor
-        #             * np.sin(-2 * relative_angle)
-        #         )
-        #         / beta
-        #     )
-        #     * np.matmul(
-        #         relative_pos,
-        #         np.matmul(np.identity(3) - matrix_M, relative_pos),
-        #     )
-        #     + (alpha_cell * eps_cell / (alpha_cell + alpha_neighbor))
-        #     * np.linalg.norm(relative_pos) ** 2
-        #     * np.sin(2 * (phies[cell_index] - theta))
-        # )
-
-        # Now that we have the force and torque we can calculate the change in velocity
-        # and orientation as it is done in the paper. Becuase of this, we need the Q
-        # matrix (already calculated) and the mobilities
-
-        # # longitudinal & transversal mobility
-        # if np.isclose(cell.aspect_ratio, 1):
-        #     mP = 1 / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        #     mS = 1 / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        # else:
-        #     mP = (
-        #         1
-        #         / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        #         * (3 * cell.aspect_ratio / 4.0)
-        #         * (
-        #             (cell.aspect_ratio) / (1 - cell.aspect_ratio**2)
-        #             + (2.0 * cell.aspect_ratio**2 - 1.0)
-        #             / np.power(cell.aspect_ratio**2 - 1.0, 1.5)
-        #             * np.log(
-        #                 cell.aspect_ratio + np.sqrt(cell.aspect_ratio**2 - 1.0)
-        #             )
-        #         )
-        #     )
-        #     mS = (
-        #         1
-        #         / np.sqrt((area * cell.aspect_ratio) / np.pi)
-        #         * (3 * cell.aspect_ratio / 8.0)
-        #         * (
-        #             (cell.aspect_ratio) / (cell.aspect_ratio**2 - 1.0)
-        #             + (2.0 * cell.aspect_ratio**2 - 3.0)
-        #             / np.power(cell.aspect_ratio**2 - 1.0, 1.5)
-        #             * np.log(
-        #                 cell.aspect_ratio + np.sqrt(cell.aspect_ratio**2 - 1.0)
-        #             )
-        #         )
-        #     )
-
-        # # rotational mobility
-        # mR = (
-        #     3
-        #     / (
-        #         2
-        #         * (area / np.pi)
-        #         * (cell.aspect_ratio + 1 / cell.aspect_ratio)
-        #     )
-        #     * mP
-        # )
-
-        # # then the change in the velocity is given by:
-        # dif_velocity = np.matmul(
-        #     ((mP + mS) / 2) * np.identity(3) + ((mP - mS) / 2) * Q_cell,
-        #     force,
-        # )
-        # # we calculate the change in the position of the cell, given all the neighbors.
-        # # Remember that the intrinsic velocity is already multiplied by the mobility
-        # # (Like in Grosmann paper).
-        # dif_position = (cell.velocity()+dif_velocity)*delta_t
-        # # and the change in the orientation:
-        # dif_phi = mR * torque * delta_t
-        # # we also add tha noise in the position:
-        # # we need the direction vectors
-        # direction_vector = np.array(
-        #     [
-        #         np.cos(phies[cell_index]),
-        #         np.sin(phies[cell_index]),
-        #         0,
-        #     ])
-        # perpendicular_vector = np.array(
-        #     [
-        #         np.cos(phies[cell_index]-np.pi/2),
-        #         np.sin(phies[cell_index]-np.pi/2),
-        #         0,
-        #     ])
-        # # and the noise
-        # s_nP = self.eta*np.sqrt(mP*delta_t)
-        # s_nS = self.eta*np.sqrt(mS*delta_t)
-
-        # nP = s_nP*np.random.normal(0, 1)
-        # nS = s_nS*np.random.normal(0, 1)
-        # noise = nP*direction_vector+nS*perpendicular_vector
         return dif_position+noise, dif_phi
